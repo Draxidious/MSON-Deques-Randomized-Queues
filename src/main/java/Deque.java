98867c57f5fe6f8a05e2d953a2f267334649b8dc
Jordan Muniz <munizj21@trinityprep.org>
2020-02-03 17:34:33 -0500

    feat: Change Linked List to doubly linked list to meet memory requirements

--- src/main/java/Deque.java
+++ src/main/java/Deque.java
@@ -19,11 +19,13 @@ import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
     private Node head;
+    private Node tail;
 
     private int size;
 
     public Deque() {
         head = null;
+        tail = null;
         size = 0;
     }
 
@@ -37,78 +39,113 @@ public class Deque<Item> implements Iterable<Item> {
 
     public void addFirst(Item item) {
         if (item == null) throw new IllegalArgumentException("tried to addFirst with null element");
+        size++;
         if (head == null) {
             head = new Node(item);
         } else {
             Node newNode = new Node(item);
             newNode.setNext(head);
+            head.setBefore(newNode);
             head = newNode;
+
         }
-        size++;
+
     }
 
     public void addLast(Item item) {
         if (item == null) throw new IllegalArgumentException("tried to addLast with null element");
+        size++;
         if (head == null) {
             head = new Node(item);
+        } else if (tail == null) {
+            tail = new Node(item);
         } else {
-            Node node = head;
-            while (node.hasNext()) {
-                node = node.getNext();
-            }
-            node.setNext(new Node(item));
+            Node newNode = new Node(item);
+            tail.setNext(newNode);
+            newNode.setBefore(tail);
+            tail = newNode;
+
         }
-        size++;
+
     }
 
     public Item removeFirst() {
         if (isEmpty()) throw new NoSuchElementException("tried to removeFirst with empty deque");
         size--;
+        if (head == null) {
+            Node ret = tail;
+            return ret.getElement();
+        }
         Node ret = head;
         head = head.getNext();
         return ret.getElement();
     }
 
     public Item removeLast() {
-        if (isEmpty()) throw new NoSuchElementException("tried to removeFirst with empty deque");
+        if (isEmpty()) throw new NoSuchElementException("tried to removeLast with empty deque");
         size--;
-        Node prev = head;
-        Node node = head;
-        while (node.hasNext()) {
-            prev = node;
-            node = node.getNext();
+        if (tail == null) {
+            Node ret = head;
+            return ret.getElement();
         }
-        prev.setNext(null);
 
-        return node.getElement();
+        Node ret = tail;
+        System.out.println(ret.getElement());
+        tail = tail.getBefore();
+        return ret.getElement();
     }
 
     public Iterator<Item> iterator() {
         // return an iterator over items in order from front to end
-       return null;
+        return null;
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Item next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+
+        }
     }
 
     public static void main(String[] args) {
         // unit testing (optional)
     }
 
-    class Node {
+    private class Node {
 
         private Item element;
         private Node next;
+        private Node before;
 
         Node(Item el) {
             element = el;
             next = null;
+            before = null;
         }
 
         Node() {
             element = null;
             next = null;
+            before = null;
         }
 
         void setNext(Node el) {
             next = el;
+        }
+
+        void setBefore(Node el) {
+            before = el;
         }
 
         public void setElement(Item el) {
@@ -123,7 +160,15 @@ public class Deque<Item> implements Iterable<Item> {
             return next;
         }
 
+        Node getBefore() {
+            return before;
+        }
+
         boolean hasNext() {
+            return next != null;
+        }
+
+        boolean hasBefore() {
             return next != null;
         }
 
